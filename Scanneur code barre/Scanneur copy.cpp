ab8f821d1bd3380ff9fe4a974a26d549d91b5043
Basile Morin <basilemorin18@gmail.com>
2024-09-23 16:02:12 +0200

    fin séance 23/09

--- Scanneur code barre/Scanneur copy.cpp
+++ Scanneur code barre/Scanneur copy.cpp
@@ -9,24 +9,29 @@ int valeurMax;
 
 int main(){
     std::ifstream fichier("photo.ppm");
-    if (fichier.is_open())
-    {                            // ouvre le fichier
+    if (fichier.is_open()) {                            // ouvre le fichier
         fichier >> format;                              // donne le format du .ppm dans la variable format
         std::cout << format << std::endl;               // print le format du fichier (ligne pour verification)
-        if (format != "P3")
-        {
+        
+        if (format != "P3") {
             std::cerr << "mauvais format" << std::endl; //envoie une erreur et arrête le programme si le format du .ppm n'est pas P3 (P3=valeur pixel numérique,P6= valeur pixel binaire donc illisible)
             return 1;
         }
-    fichier >> largeur >> longueur >> valeurMax;        //stocke la longueur et la largeur du tableau et la valeur max des couleurs
-    std::cout << largeur << " " << longueur << " " << valeurMax << std::endl; 
-    //int p //faire tableau de tableau
-    for (i=1 ; i<(longueur/2) ; i++) 
-    {
-
-
-    }
-
+        
+        fichier >> largeur >> longueur >> valeurMax;        //stocke la longueur et la largeur du tableau et la valeur max des couleurs
+        std::cout << largeur << " " << longueur << " " << valeurMax << std::endl; 
+        int** pValeur =  new int*[largeur]; //faire tableau de tableau
+        
+        for (int i=0; i<largeur; i++){
+            pValeur[i] = new int[3];
+        
+        }
+        for (int j=1; j<(longueur/2)-1; j++){
+            for (int i=0; i<largeur; i++){
+                fichier >> pValeur[i][0] >> pValeur[i][1] >> pValeur[i][2];
+                std::cout << pValeur[i][0] << " " << pValeur[i][1] << " " << pValeur[i][2] << std::endl;
+            }
+        }
 
 
 
@@ -37,9 +42,17 @@ int main(){
     //}
     
     
+
+
+
     fichier.close();
+
+    for (int i = 0; i < largeur; i++) {
+        delete[] pValeur[i];  // Libération des colonnes
+        }
+        delete[] pValeur;  // Libération des lignes
+    }
 }
 
 
 
-}
