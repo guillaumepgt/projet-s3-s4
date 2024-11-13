bc7242602dea8530f87b21d93e9fa1ede7c16ca6
Basile Morin <basilemorin18@gmail.com>
2024-11-13 16:31:49 +0100

    séance 13/11

--- Scanneur code barre/Scanneur stock pas les valeurs.cpp
+++ Scanneur code barre/Scanneur stock pas les valeurs.cpp
@@ -49,7 +49,6 @@
                 {                                
                     fichierLigne << pValeur[j] <<" ";                    //on écrit ces 3 valeurs dans fichierligne pour avoir un fichier qui donne le résultat
                 }
-                fichierLigne << " " ;
             }
             
             fichier.close();
@@ -71,11 +70,93 @@
 
     }
 
+    int detecLargeurColonne()
+    {
+        int valeur = 255;
+        int largeurRef=0;
+        int compteur=0;
+        int largeurBande=0;
+        int i=0;
+        std::ifstream fichier("Ligne code barre.txt");
+        if (fichier.is_open())       // ouvre le fichier
+        {
+            while (valeur>=128)  // On va jusqu'au début du code barre(nécessite que le code barre soit sur fond blanc)
+            {
+                fichier >> valeur;
+            }
+
+
+            while (valeur<128)   //lecture de la premiere barre qui doit être noir puis on stock sa longueur (doit être un multiple de 3 car un pixel=3valeurs)
+            {
+                fichier >> valeur;
+                compteur++;
+            }
+
+
+            std::cout << compteur << "largeurRef" << std::endl;
+            largeurRef=compteur;
+            int* pCode = new int[67];
+            pCode[0]=0;
+
+            while (i<=67)
+            {
+                compteur=0;
+                if (valeur>=128)
+                {
+                    while (valeur>=128)   //lecture de la premiere barre qui doit être noir puis on stock sa longueur (doit être un multiple de 3 car un pixel=3valeurs)
+                    {
+                        fichier >> valeur;
+                        compteur++;
+                    }
+                } else
+                {
+                    while (valeur<128)   //lecture de la premiere barre qui doit être noir puis on stock sa longueur (doit être un multiple de 3 car un pixel=3valeurs)
+                    {
+                        fichier >> valeur;
+                        compteur++;
+                    }            
+                }
+                largeurBande=compteur/largeurRef;
+                if (compteur%largeurRef>0)
+                {
+                    largeurBande++;
+                }
+                for (int j=i; j<=i+largeurBande-1; j++)
+                {
+                    if (pCode[i-1]=0) {pCode[j]=1;}
+                    if (pCode[i-1]=1) {pCode[j]=0;}
+                }
+                i=i+largeurBande;
+
+            }
+            for (int j =0; j<67; j++)
+            {
+                std::cout<< pCode[j] << std::endl;
+            }
+
+            
+            
+            
+            return 1;
+
+
+
+
+        } else
+        {
+            std::cerr << "Impossible d'ouvrir le fichier." << std::endl;
+            return 1;
+        }
+
+
+    }
+
 
     int main()
     {
-        std::cout  << recupLigne() << std::endl;
+        std::cout << detecLargeurColonne()<< std::endl;
     }
+
 
 
 
