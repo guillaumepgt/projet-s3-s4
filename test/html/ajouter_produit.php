e9250eb570fc330ca4799e7a2bcb2565feedfd9b
guillaumepgt <guillaume.prigent44@gmail.com>
2025-04-28 15:53:56 +0200

    fonctionnel

--- test/html/ajouter_produit.php
+++ test/html/ajouter_produit.php
@@ -3,37 +3,38 @@ $host = 'mysql';
 $dbname = 'database';
 $user = 'project';
 $password = '12345678';
+
 // Vérifiez si le formulaire a été soumis
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupérez les données envoyées depuis le formulaire
-    $nom_produit = isset($_POST['nom_produit']) ? $_POST['nom_produit'] : '';
-    $prix_produit = isset($_POST['prix_produit']) ? $_POST['prix_produit'] : '';
+    $nom = isset($_POST['nom']) ? $_POST['nom'] : '';
+    $prix = isset($_POST['prix']) ? $_POST['prix'] : '';
     $code_barre = isset($_POST['code_barre']) ? $_POST['code_barre'] : '';
 
     // Vérifiez que tous les champs requis sont remplis
-    if (!empty($nom_produit) && !empty($prix_produit) && !empty($code_barre)) {
+    if (!empty($nom) && !empty($prix) && !empty($code_barre)) {
         // Connectez-vous à la base de données (remplacez par vos informations)
-
         try {
             // Création de la connexion PDO
             $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             // Préparez la requête SQL pour insérer les données
+
             $stmt = $pdo->prepare(
-                'INSERT INTO produits (nom_produit, prix_produit, code_barre) 
-                 VALUES (:nom_produit, :prix_produit, :code_barre)'
+                'INSERT INTO produits (nom, prix, code_barre) 
+                 VALUES (:nom, :prix, :code_barre)'
             );
 
             // Exécutez la requête avec les données du formulaire
             $stmt->execute([
-                ':nom_produit' => $nom_produit,
-                ':prix_produit' => $prix_produit,
-                ':code_barre' => $code_barre,
+                ':nom' => htmlspecialchars($nom),
+                ':prix' => htmlspecialchars($prix),
+                ':code_barre' => htmlspecialchars($code_barre),
             ]);
 
             // Message de succès
-            echo "<p>Le produit <strong>" . htmlspecialchars($nom_produit) . "</strong> a été ajouté avec succès !</p>";
+            echo "<p>Le produit <strong>" . htmlspecialchars($nom) . "</strong> a été ajouté avec succès !</p>";
         } catch (PDOException $e) {
             // Gérer les erreurs de connexion ou d'exécution
             echo "<p>Erreur : " . htmlspecialchars($e->getMessage()) . "</p>";
--- test/html/api/data.php
+++ test/html/api/data.php
@@ -1,14 +1,14 @@
 <?php
 // Configuration de la base de données
-$host = 'mysql';
-$dbname = 'database';
-$user = 'project';
-$password = '12345678';
 
 // Vérification des paramètres
-if (empty($_GET['nom']) && empty($_GET['prenom']) && empty($_GET['email'])) {
+if (empty($_GET['nom']) && empty($_GET['prenom']) && empty($_GET['email']) && empty($_GET['code_barre'])) {
     http_response_code(400);
     echo json_encode(['erreur' => "Au moins un des paramètres 'nom', 'prenom' ou 'email' est requis"], JSON_UNESCAPED_UNICODE);
+    exit;
+    } else if (isset($_GET['code_barre']) && empty($_GET['nom']) && empty($_GET['prenom']) && empty($_GET['email'])) {
+    code_barre(isset($_GET['code_barre']) ? htmlspecialchars($_GET['code_barre']) : '');
+    http_response_code(200);
     exit;
 }
 
@@ -19,8 +19,7 @@ $email = isset($_GET['email']) ? htmlspecialchars($_GET['email']) : '';
 
 try {
     // Connexion à la base de données
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = database();
 
     // Requête avancée avec LIKE et SOUNDEX
     $sql = "SELECT prenom, nom, code_postal, email 
@@ -51,3 +50,44 @@ try {
     echo json_encode(['erreur' => "Erreur de connexion à la base de données : " . $e->getMessage()], JSON_UNESCAPED_UNICODE);
 }
 
+function code_barre($code_barre)
+{
+    try {
+        // Connexion à la base de données
+        $pdo = database();
+
+        // Requête avancée avec LIKE et SOUNDEX
+        $sql = "SELECT nom, prix
+                FROM produits
+                WHERE code_barre = :code_barre";
+
+        // Préparation et exécution de la requête
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            'code_barre' => $code_barre,
+        ]);
+
+        // Récupération des résultats
+        $resultats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if (empty($resultats)) {
+            echo json_encode(['message' => "Aucun produits trouvé"], JSON_UNESCAPED_UNICODE);
+        } else {
+            echo json_encode($resultats, JSON_UNESCAPED_UNICODE);
+        }
+    } catch (PDOException $e) {
+        http_response_code(500);
+        echo json_encode(['erreur' => "Erreur de connexion à la base de données : " . $e->getMessage()], JSON_UNESCAPED_UNICODE);
+    }
+}
+
+function database()
+{
+    $host = 'mysql';
+    $dbname = 'database';
+    $user = 'project';
+    $password = '12345678';
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+}
